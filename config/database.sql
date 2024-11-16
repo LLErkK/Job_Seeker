bb7fd2e8db06f1224ac6f082888602b309a090c5
Rozaqatg <160761434+Rozaqatg@users.noreply.github.com>
2024-11-16 23:58:42 +0700

    fix database

--- config/database.sql
+++ config/database.sql
@@ -1,49 +1,49 @@
 CREATE DATABASE job_portal;
+
 CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    role ENUM('Job Seeker', 'Admin', 'Employer') NOT NULL
+    role ENUM('job_seeker', 'admin', 'employer') NOT NULL
 );
+
 CREATE TABLE profile (
     profile_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     name VARCHAR(100) DEFAULT NULL,
     umur INT,
-    gender ENUM,
+    gender ENUM('male', 'female', 'other') NOT NULL, -- assuming gender options
     alamat TEXT,
     riwayat_pendidikan TEXT,
     riwayat_pekerjaan TEXT,
     deskripsi TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    photo VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-CREATE TABLE Job (
-    job_id INT PRIMARY KEY AUTO_INCREMENT,
+
+CREATE TABLE jobs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     employer_id INT NOT NULL,
     title VARCHAR(100) NOT NULL,
     description TEXT,
-    job_type ENUM('Intern', 'Full-Time') NOT NULL,
+    job_type ENUM('intern', 'full-time') NOT NULL,
     status ENUM('Open', 'Closed') NOT NULL,
-    requirments TEXT,
+    requirements TEXT,
     salary DOUBLE(10,2),
     location TEXT,
     deadline DATE,
-    company_name VARCHAR,
-    FOREIGN KEY (employer_id) REFERENCES users(user_id)
-    FOREIGN KEY (company_name) REFERENCES users(username)
+    company_name VARCHAR(100), -- assumed max length for company name
+    FOREIGN KEY (employer_id) REFERENCES users(id) ON DELETE CASCADE, 
+    FOREIGN KEY (company_name) REFERENCES users(username) ON DELETE CASCADE
 );
 
-
-
-
-
-CREATE TABLE Application (
-    application_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE job_applications (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     job_id INT NOT NULL,
     user_id INT NOT NULL,
     application_date DATE,
-    status ENUM('Pending', 'Accepted', 'Rejected') DEFAULT 'Pending',
-    FOREIGN KEY (job_id) REFERENCES Job(job_id),
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+    status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+    FOREIGN KEY (job_id) REFERENCES jobs(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
